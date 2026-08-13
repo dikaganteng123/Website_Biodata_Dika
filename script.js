4e59bfb828cb9170acf0f75730bad86567f7567b
Dika <dikaanggarasantosa5576@gmail.com>
2026-08-13 11:18:02 +0700

    ke duplikat

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function openHobby(hobby) {
     let card = document.getElementById(hobby);
     card.classList.add("active");
@@ -7,14 +6,5 @@ function openHobby(hobby) {
 function closeHobby(hobby) {
     let card = document.getElementById(hobby);
     card.classList.remove("active");
-=======
-function openHobby(hobby) {
-    let card = document.getElementById(hobby);
-    card.classList.add("active");
-}
 
-function closeHobby(hobby) {
-    let card = document.getElementById(hobby);
-    card.classList.remove("active");
->>>>>>> ee231887ec3811b3c1afa0d2d06485d4ef3b1e1e
 }
